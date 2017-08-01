f1e1e87ad919e6461d7848223e91397a49b1bc1e
Gordon <gordon.wallac3@yahoo.com>
2017-08-01 17:37:22 -0600

    added styles to the document page and added the styles folder

--- freelance-camp-fe/app/documents/documents.component.js
+++ freelance-camp-fe/app/documents/documents.component.js
@@ -15,22 +15,22 @@ var DocumentsComponent = (function () {
                 title: 'my first doc',
                 description: 'asdfasf',
                 file_url: 'google.com',
-                updated_at: '123',
-                image_url: 'http://google.com'
+                updated_at: '12/3',
+                image_url: 'https://c1.staticflickr.com/7/6129/5964208782_8bb300bbaa_b.jpg'
             },
             {
                 title: 'my second doc',
                 description: 'asdfasf',
                 file_url: 'google.com',
                 updated_at: '123',
-                image_url: 'http://google.com'
+                image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Traditional_American_taco_-_Evan_Swigart.jpg/1280px-Traditional_American_taco_-_Evan_Swigart.jpg'
             },
             {
                 title: 'my third doc',
                 description: 'asdfasf',
                 file_url: 'google.com',
                 updated_at: '123',
-                image_url: 'http://google.com'
+                image_url: 'https://c1.staticflickr.com/8/7003/6757470443_78bfba1c69_b.jpg'
             },
         ];
     }
@@ -38,7 +38,8 @@ var DocumentsComponent = (function () {
         core_1.Component({
             moduleId: module.id,
             selector: 'documents',
-            templateUrl: 'documents.component.html'
+            templateUrl: 'documents.component.html',
+            styleUrls: ['documents.component.css']
         })
     ], DocumentsComponent);
     return DocumentsComponent;
--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -4,7 +4,8 @@ import { Document } from './document';
 @Component({
 	moduleId: module.id,
 	selector: 'documents',
-	templateUrl: 'documents.component.html'
+	templateUrl: 'documents.component.html',
+	styleUrls: ['documents.component.css']
 }) 
 
 export class DocumentsComponent {
@@ -14,22 +15,22 @@ export class DocumentsComponent {
 			title: 'my first doc',
 			description: 'asdfasf',
 			file_url: 'google.com',
-			updated_at: '123',
-			image_url: 'http://google.com'
+			updated_at: '12/3',
+			image_url: 'https://c1.staticflickr.com/7/6129/5964208782_8bb300bbaa_b.jpg'
 		},
 		{ 
 			title: 'my second doc',
 			description: 'asdfasf',
 			file_url: 'google.com',
 			updated_at: '123',
-			image_url: 'http://google.com'
+			image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Traditional_American_taco_-_Evan_Swigart.jpg/1280px-Traditional_American_taco_-_Evan_Swigart.jpg'
 		},
 		{ 
 			title: 'my third doc',
 			description: 'asdfasf',
 			file_url: 'google.com',
 			updated_at: '123',
-			image_url: 'http://google.com'
+			image_url: 'https://c1.staticflickr.com/8/7003/6757470443_78bfba1c69_b.jpg'
 		},
 	]
 }
